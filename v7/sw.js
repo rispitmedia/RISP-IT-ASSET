a6941a9d3cb86f033cf376d1b09541c69ab3bf46
rispitmedia <itmedia@risphuket.ac.th>
2026-09-24 00:18:07 +0700

    Add files via upload

--- v7/sw.js
+++ v7/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME='risp-it-asset-v7-step3-full-control-1';
+const CACHE_NAME='risp-it-asset-v7-step3-1-pro-control-1';
 const CORE=[
-  './','./index.html','./styles.css?v=730','./app.js?v=730','./manifest.webmanifest?v=730',
+  './','./index.html','./styles.css?v=741','./app.js?v=741','./manifest.webmanifest?v=741',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
